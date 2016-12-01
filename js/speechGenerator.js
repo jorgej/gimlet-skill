60cccb565f24bc9b391c19581c0e0c2c40f8478d
Adam Larsen <adam@conversantlabs.com>
2016-12-01 16:47:34 -0500

    Initial implementation of speechGenerator

--- js/speechGenerator.js
+++ js/speechGenerator.js
@@ -1,6 +1,41 @@
-module.exports = function(key) {
-    // ... return something depending on the key (and maybe fill in the args?) ...
-    return createSSMLSpeechObjectFromText("testing 1 2 3");
+module.exports = function(key, ...args) {
+
+    var text;
+
+    switch (key) {
+        case "ReplyAllIntroduction":
+            text = "Check out this episode of Reply All!";
+            break;
+        case "PlayingShow":
+            if (args.length == 1) {
+                var nameOfShow = args[0];
+                text = `Playing ${nameOfShow}.`;
+            }
+            else {
+                console.log(`Error: speechGenerator's PlayingShow case was expecting one argument (found ${args.length} arguments).`);
+                // TODO: Handle unexpected number of arguments (e.g. none). Should this be an assert, since it would be a programmer error?
+                return undefined;
+            }
+            break;
+        case "SomethingWithManyArguments":  // TODO: Remove this sample case
+            if (args.length > 0) {
+                text = `Let me tell you about ${args[0]}`;
+                args.forEach(function(arg, index) {
+                    if (index > 0) { // Skip the first argument, since we included it in the initial line above.
+                        text += `and ${arg}`;
+                    }
+                });
+            }
+            else {
+                // TODO: Handle unepxected number of arguments
+            }
+            break;
+        default:
+            // TODO: Handle an invalid key.
+            return undefined;
+    }
+
+    return createSSMLSpeechObjectFromText(text);
 };
 
 function createSSMLSpeechObjectFromText(text) {
