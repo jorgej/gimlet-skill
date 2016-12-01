15ca48e6c6e2a5e40e39dcf42db60298659597d6
Greg Nicholas <greg.nicholas@gmail.com>
2016-12-01 16:08:08 -0500

    change speechGenerator export

--- js/speechGenerator.js
+++ js/speechGenerator.js
@@ -1,8 +1,6 @@
-module.exports = {
-    generate: function(key) {
-        // ... return something depending on the key (and maybe fill in the args?) ...
-        return createSSMLSpeechObjectFromText("testing 1 2 3");
-    }
+module.exports = function(key) {
+    // ... return something depending on the key (and maybe fill in the args?) ...
+    return createSSMLSpeechObjectFromText("testing 1 2 3");
 };
 
 function createSSMLSpeechObjectFromText(text) {
