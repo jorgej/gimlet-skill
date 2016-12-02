ea7a244004c96621f22194e387732c76e336b204
Adam Larsen <adam@conversantlabs.com>
2016-12-02 13:11:25 -0500

    Simplify speechGenerator

--- js/speechGenerator.js
+++ js/speechGenerator.js
@@ -1,52 +1,22 @@
 module.exports = function(key, ...args) {
-
-    var text;
-
-    switch (key) {
-        case "ReplyAllIntroduction":
-            text = "Check out this episode of Reply All!";
-            break;
-        case "PlayingShow":
-            if (args.length === 1) {
-                var nameOfShow = args[0];
-                text = `Playing ${nameOfShow}.`;
-            }
-            else {
-                logUnexpectedArgumentsCount(key, args.length, 1);
-                // TODO: Should this be an assert, since it would be a programmer error?
-                return undefined;
-            }
-            break;
-        case "SomethingWithManyArguments":  // TODO: Remove this sample case
-            if (args.length > 0) {
-                text = `Let me tell you about ${args[0]}`;
-                args.forEach(function(arg, index) {
-                    if (index > 0) { // Skip the first argument, since we included it in the initial line above.
-                        text += ` and ${arg}`;
-                    }
-                });
-            }
-            else {
-                logUnexpectedArgumentsCount(key, args.length, "1 or more");
-                // TODO: Should this be an assert, since it would be a programmer error?
-                return undefined;
-            }
-            break;
-        default:
-            console.log(`Error: speechGenerator encountered unexpected key "${key}"`);
-            return undefined;
-    }
-
-    return createSSMLSpeechObjectFromText(text);
+    var speech = {
+        "ReplyAllIntroduction":     text(`Check out this episode of Reply All!`),
+        "CurrentlyPlayingEpisode":  text(`Now playing episode ${args[0]} of ${args[1]}`),
+        "SomethingWithAudio":       audio(`https://carfu.com/audio/carfu-welcome.mp3`)
+    };
+    return speech[key];
 };
 
-function createSSMLSpeechObjectFromText(text) {
+function text(text) {
     return {
         type: 'SSML',
         ssml: `<speak> ${text} </speak>`
     };
 }
 
-function logUnexpectedArgumentsCount(key, argsCount, expectedArgsCount) {
-    console.log(`Error: speechGenerator encountered ${argsCount} argument(s), but was expecting ${expectedArgsCount} argument(s) for key "${key}".`);
+function audio(url) {
+    return {
+        type: 'SSML',
+        ssml: `<speak> <audio src="${url}" /> </speak>`
+    };
 }
