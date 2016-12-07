f2723d50eea23ccf25975c83ea74245ad17694db
Greg Nicholas <greg.nicholas@gmail.com>
2016-12-06 22:45:49 -0500

    create build script for "production" skill

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf _build
+mkdir _build
+
+echo 'Copying files...'
+cp -r js _build/src
+cd _build/src
+
+echo 'Inserting production skill ID...'
+sed -i '.bak' 's/fb7cbf45-1f45-4307-b2ce-b36bc871625f/a16e1bcb-690b-470d-97a8-061251c559e0/g' constants.js
+rm constants.js.bak
+
+echo 'Compressing files...'
+zip -rq ../package.zip *
+cd ../..
+
+echo 'Done. File is at `_build/package.zip`'
